7c682f98ee4764c9d86d59231f456a8252d5bb9b
a.a.efimov <paterm@ya.ru>
2021-08-02 21:16:16 +0500

    appending in results.txt resume working write file in process

--- index.js
+++ index.js
@@ -9,27 +9,23 @@ async function start() {
     output: process.stdout
   })
 
-  rl.question('Откуда берем ссылки?\n Из файла (F) или с сайта (S)', async (variant) => {
-    if (variant === 'f' || 'F') {
+  rl.question('Очистить файл с результатами? (Y/n)', async (clearFile) => {
+    if (!clearFile || clearFile.toLowerCase() === 'y') {
+      fs.truncate('result.txt', 0, () => console.log('Файл был очищен'))
+    }
+
+    rl.question('Укажи номер с какой ссылки начать проверку (по умолчанию = 1)', async (startFrom) => {
       try {
         const filePath = await getFilePath()
         const links = await parseFile(filePath)
-        // const links = [
-        //   'https://b2btrade.ru/product-sladkie-grezy-oreshki-so-sguschenkoy',
-        //   'https://b2btrade.ru/catalog/product-east-bali-cashews-elektricheskiy-shto',
-        //   'https://b2btrade.ru/asd'
-        // ]
-        const result = await checkLinks(links)
-        writeInFile(result)
+        await checkLinks(links, +startFrom)
         console.log('Результат работы записан в файл result.txt')
       } catch (e) {
         console.error(e)
       }
-    } else {
-      parseSite()
-    }
 
-    rl.close()
+      rl.close()
+    })
   })
 }
 
@@ -51,6 +47,10 @@ function parseFile(filePath) {
   return new Promise((resolve, reject) => {
     if (!filePath) reject('Путь к файлу не может быть пустым')
 
+    if (!fs.existsSync(filePath)) {
+      reject('Некорректный путь до файла')
+    }
+
     fs.readFile(filePath, 'utf8', (err, data) => {
       const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gm
       const string = data.replaceAll(/\r?\n|\r/g, '')
@@ -61,73 +61,63 @@ function parseFile(filePath) {
   })
 }
 
-async function checkLinks(links) {
-  const result = {
-    success: [],
-    redirect: [],
-    error: []
-  }
-  let iterator = 1
+async function checkLinks(links, startFrom) {
+  const file = fs
+    .createWriteStream('result.txt', { flags:'as+' })
+    .on('error', (e) => console.error(e))
+
+  let iterator = startFrom || 1
 
   if (!links || !links.length) {
-    return result
+    return null
   }
 
-  for await (const link of links) {
+  const reducesLinks = links.slice(iterator - 1, links.length)
+
+  console.log('links.length', links.length)
+  console.log('reducesLinks.length', reducesLinks.length)
+
+  for await (const link of reducesLinks) {
+    console.log(`Link ${iterator} of ${links.length}`)
     try {
       const res = await axios.get(link)
-      console.log(`Link ${iterator} of ${links.length}`)
 
       if (res.request?._redirectable?._redirectCount) {
         console.log('Link were redirected:', link)
-        result.redirect.push({ url: link, status: res.status, redirect: true })
+        writeLine(file, { url: link, status: res.status, redirect: true })
       } else {
-        console.log('Link success:', link)
-        result.success.push({ url: link, status: res.status })
+        console.log('Link success')
+        // writeLine(file, { url: link, status: res.status })
       }
 
-      console.log('res.status', res.status)
+      console.log('Status', res.status)
     } catch (e) {
       console.error('Link error:', link)
-      result.error.push({
+      writeLine(
+        file,
+        {
         url: link,
         status: e.response?.status,
         text: e.response?.statusText
       })
     }
 
+    console.log('--------')
     iterator++
   }
 
-  return result
-}
-
-function writeInFile(resultArray) {
-  if (!resultArray) {
-    return null
-  }
-
-  const file = fs.createWriteStream('result.txt')
-    .on('error', (e) => console.error(e))
-
-  Object.keys(resultArray).forEach(key => {
-    file.write(key.toUpperCase() + ':\n')
-    for (const item of resultArray[key]) {
-      let str = item.url
-
-      if (item.status) str += `, Code: ${item.status}`
-      if (item.redirect) str += `, Redirect`
-      if (item.text) str += `, Message: ${item.text}`
-
-      file.write(str + '\n')
-    }
-  })
-
   file.end()
+  return null
 }
 
-async function parseSite() {
-  console.log('тут нужно дописать функционал конечно')
+function writeLine(file, data) {
+  let str = data.url
+
+  if (data.status) str += `, Code: ${data.status}`
+  if (data.redirect) str += `, Redirect`
+  if (data.text) str += `, Message: ${data.text}`
+
+  file.write(str + '\n')
 }
 
 start()
